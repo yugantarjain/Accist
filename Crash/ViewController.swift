59215f31527c48e5dd287dc9f260ea58a6b47f8e
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-17 23:22:59 +0530

    Map integrated. Zoom enabled.

--- Crash/ViewController.swift
+++ Crash/ViewController.swift
@@ -9,13 +9,16 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        map.isZoomEnabled = true
+        
     }
 
-
+    @IBOutlet weak var map: MKMapView!
 }
 
