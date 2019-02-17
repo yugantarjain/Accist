bdfbeef6ae7debdc2bcb16d5027b7fec43f8b832
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-18 01:21:09 +0530

    Maps integrated. Initial location and region defined.

--- Crash/FullViewController.swift
+++ Crash/FullViewController.swift
@@ -16,6 +16,13 @@ class ViewController: UIViewController
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         map.isZoomEnabled = true
+//        map.centerCoordinate.latitude = 28
+//        map.centerCoordinate.longitude = 77
+        let centreCoord = CLLocationCoordinate2D(latitude: 28, longitude: 77)
+        let span = MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5)
+        map.region = MKCoordinateRegion(center: centreCoord, span: span)
+       
+        
         
     }
 
