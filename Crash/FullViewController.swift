e10d4e60b1e1c9f129b052144ae3ed6c8010c6d2
Yugantar Jain <yugantar8jain@gmail.com>
2019-02-18 07:32:21 +0530

    Map to be done.

--- Crash/FullViewController.swift
+++ Crash/FullViewController.swift
@@ -25,10 +25,36 @@ class ViewController: UIViewController, WKUIDelegate
         // Do any additional setup after loading the view, typically from a nib.
         resolvedButton.layer.cornerRadius = 5
         
+        //image load
+        let myURL = URL(string: self.imageLink)
+        let myRequest = URLRequest(url: myURL!)
+        image.load(myRequest)
+        
+        //place, lati, longi, time
+        location.text = place
+        latit.text = String(lati)
+        longit.text = String(longi)
+        timet.text = time
+        
+        //map
+        let xy = CLLocation(latitude: lati, longitude: longi)
+        
+
+        
     }
 
     @IBOutlet weak var image: WKWebView!
     @IBOutlet weak var resolvedButton: UIButton!
     @IBOutlet weak var map: MKMapView!
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var latit: UILabel!
+    @IBOutlet weak var longit: UILabel!
+    @IBOutlet weak var timet: UILabel!
+    
+    
+    
+    
+    
+    
 }
 
